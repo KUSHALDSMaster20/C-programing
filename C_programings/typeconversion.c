32056a3e32a2c17b88f5585c6aeda070e274e880
kushal.DS <kushal998081@gmail.com>
2025-09-13 23:56:42 +0530

    type conversion file is added

---
+++ C_programings/typeconversion.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ 
+int main(){
+    /*in this case ther one float value and int
+     value is ther so in the case we use float data type not int 
+     bicose it return the float value not int */
+     
+    printf("%f \n ",2.0*2);
+    return 0;
+}
