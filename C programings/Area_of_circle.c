86805cdcfec053895573660cad1491293c4c79c5
kushal.DS <kushal998081@gmail.com>
2025-09-13 23:23:21 +0530

    modefiead2

--- C programings/Area_of_circle.c
+++ C programings/Area_of_circle.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include<math.h>
 int main(){
     float pi=3.14,r;
     printf("Enter the redius :- ");
     scanf("%f",&r);
-    printf("area of circle is :- %f",pi*powe(r,2));
+    printf("area of circle is :- %f",pi*r*r);
     return 0;
 }
 
